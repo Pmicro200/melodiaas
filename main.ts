359d7168e8fdb419c0ef39ef5211496c121d99e5
Pmicro200 <98743108+Pmicro200@users.noreply.github.com>
2022-02-02 09:17:09 +0100

    haciendo melodia

--- main.ts
+++ main.ts
@@ -1,10 +1,20 @@
-basic.showLeds(`
-    # . # . #
-    . # . # .
-    # . # . #
-    . # . # .
-    # . # . #
-    `)
+music.playTone(330, music.beat(BeatFraction.Half))
+music.playTone(392, music.beat(BeatFraction.Half))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Half))
+music.playTone(494, music.beat(BeatFraction.Half))
+music.playTone(523, music.beat(BeatFraction.Double))
+music.playTone(523, music.beat(BeatFraction.Whole))
+music.playTone(523, music.beat(BeatFraction.Whole))
+music.playTone(494, music.beat(BeatFraction.Whole))
+music.playTone(494, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.playTone(392, music.beat(BeatFraction.Whole))
+music.playTone(392, music.beat(BeatFraction.Whole))
+music.playTone(440, music.beat(BeatFraction.Whole))
+music.rest(music.beat(BeatFraction.Whole))
+music.playTone(330, music.beat(BeatFraction.Half))
 basic.forever(function () {
-	
+    basic.showIcon(IconNames.EigthNote)
 })
